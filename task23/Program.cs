2304b3d169f7129e0f733c68f91523302b6df4f9
Victoria <tikhomirovavs@gmail.com>
2022-11-20 18:36:39 +0300

    Закончили 23 задачу

---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int n)
+{
+    int index = 1;
+    while (index <= n)
+    {
+    int cube = index * index * index;
+    Console.WriteLine($"{index, 5} {cube, 5}");
+    index++;
+    }
+}
+
+if (N > 0) Cube(N);
+else Console.WriteLine("Вы ввели не натуральное число");
