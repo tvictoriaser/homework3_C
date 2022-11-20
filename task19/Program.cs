304d4a3ab29b8b0e617cdb755ae2d5becb0d6a91
Victoria <tikhomirovavs@gmail.com>
2022-11-20 20:34:49 +0300

    Закончили 19 задачу

---
+++ task19/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Palindrome (int n)
+{
+    return n / 10000 == n % 10 && n / 1000 % 10 == n / 10 % 10;
+}
+
+if (number > 9999 && number < 100000)
+{
+    bool result = Palindrome(number);
+    Console.WriteLine(result ? "Да" : "Нет");
+}
+else Console.WriteLine("Вы ввели неверное число");
+
